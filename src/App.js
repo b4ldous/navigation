2316128421ebfd4c010a9580f6898bcb2f5d3bc8
Ubaldo Barranco <baldous@protonmail.com>
2022-05-20 18:23:47 -0500

    basic nested routes template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import First from "./pages/First";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>} />
+        <Route path="first" element={<First/>} />
+              
+      </Route>
+      <Route path="*" element={<NoPage/>} />
+    </Routes>
+  </BrowserRouter>
+    
+     
+    </>
   );
 }
 
---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoPage = () => {
+  return (
+    <div>NoPage (always add a link to homepage)</div>
+  )
+}
+
+export default NoPage
