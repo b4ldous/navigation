2cf2844c788d84b38c94bfb33baa1c9f8a2d5b04
Ubaldo Barranco <baldous@protonmail.com>
2022-05-20 22:29:32 -0500

    A simple drawer with active link

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,95 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import React, { useState } from "react";
+  import { AppBar, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Box} from "@mui/material";
+  import MenuIcon from '@mui/icons-material/Menu';
+  import HomeIcon from '@mui/icons-material/Home';
+import { Outlet, NavLink } from "react-router-dom";
 
 const Layout = () => {
+
+  const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
+    <Drawer
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        
+
+      >
+
+        <Box sx={{width: '300px'}} >
+
+
+        
+          <List>
+
           
-        </ul>
-      </nav>
+         
+<ListItem onClick={() => setOpenDrawer(false)}>
+  <ListItemText>
+    <NavLink to="/first"   style={({ isActive }) => ({
+color: isActive ? '#fff' : '#545e6f',
+background: isActive ? '#7600dc' : '#f0f0f0',
+})}>First</NavLink>
+  </ListItemText>
+</ListItem>
+
+
+</List>
+
+
+
+
+        
+        
+        </Box>
+        
+      </Drawer>
+
+      <Outlet />
+
+      <AppBar position="fixed" color="inherit">
+
+        
+
+
+        <Toolbar>
+        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+        <MenuIcon/>
+      </IconButton>
+
+      <IconButton  to='/' component={NavLink}> 
+      <HomeIcon />
+
+      </IconButton>
+      
+
+        </Toolbar>
+      
+
+        
+        
+
+
+        
+
+        
+
+      
+
+
+      </AppBar>
+
+      
+
+
+
+
+
+
+
+
+
+
 
       <Outlet />
     
