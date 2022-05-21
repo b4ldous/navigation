90c95df630df5d05b1d546c38914f3cfcd4176d1
Ubaldo Barranco <baldous@protonmail.com>
2022-05-20 22:57:04 -0500

    simple menu

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,21 +1,71 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { AppBar, Typography } from '@mui/material';
+
 
 const Layout = () => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
-          
-        </ul>
-      </nav>
+<AppBar>
+<Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <Typography variant='body' sx={{color: 'white'}}>Menu</Typography>
+      </Button>
 
+
+</AppBar>
+
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+
+          <NavLink to='/'> Home</NavLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+
+        <NavLink to='/first'> first</NavLink>
+        </MenuItem>
+        
+      </Menu>
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
       <Outlet />
     
     
