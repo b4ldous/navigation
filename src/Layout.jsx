18fff836d3ab8c75eb1acc095537eaefb58d1fcd
Ubaldo Barranco <baldous@protonmail.com>
2022-05-20 21:30:18 -0500

    a simple navBar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,53 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
+import { Outlet, NavLink } from "react-router-dom";
+import { Typography } from '@mui/material';
 
 const Layout = () => {
   return (
-    <>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
-          
-        </ul>
-      </nav>
 
-      <Outlet />
+
+
+    <>
+    <AppBar color='inherit' position='fixed' elevation={1}>
+
+      <Container fixed>
+        <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+
+          <Box component={NavLink} to='/' sx={{color: 'black', textDecoration: 'none'}}>
+            <Typography variant='h6' >Logo</Typography>
+
+          </Box>
+          
+          
+          <Box>
+            <Box component={NavLink} to='first' sx={{textDecoration: 'none',color: 'black', '&:hover': {background: 'grey'} }} style={({ isActive }) => ({
+
+background: isActive ? '#f0f5f5' : '', borderBottom: isActive ? '1' : ''
+})}>
+              <Typography variant='body'>First</Typography>
+            </Box>
+
+
+            
+          </Box>
+
+
+       </Box>
+
+
+
+      </Container>
+
+
+      
+    </AppBar>
+    
+
+    <Outlet />
     
     
     </>
