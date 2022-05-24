fba20bf55da99187e99af95d3e2a7b2e553e6d6b
Ubaldo Barranco <baldous@protonmail.com>
2022-05-24 11:42:37 -0500

    navbar with active link

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,27 +1,90 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Typography  from '@mui/material/Typography';
+import Box from '@mui/material/Box'
+
+
+const navLinks = [
+  { title: "About", path: "/first" }, {
+    title: "Contact", path: '/second'
+  }];
+
+
+
 
 const Layout = () => {
   return (
     <>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
-          <li>
-            <Link to="/second">Second</Link>
-          </li>
-        </ul>
-      </nav>
+     <AppBar color='inherit' elevation={0}>
+      <Container fixed>
 
-      <Outlet />
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', height: '75px', alignItems: 'center'}}>
+          <Box sx={{textDecoration: 'none', color: 'black'}} component={NavLink} to='/'>
+            <Typography variant='h5'>Home</Typography>
+
+          </Box>
+          
+          
+          <Box sx={{margin: 0}}>
+
+
+          {navLinks.map(({ title, path }) => (
+                <Box sx={{textDecoration: 'none', color: 'black', padding: '10px'}} key={title} component={NavLink} to={path} style={({ isActive }) => ({
+                 
+                  background: isActive ? '#ebebe0' : '',
+                  borderRadius: isActive ? '10px': ''
+                })} >
+                  <Typography variant='body'>{title}</Typography>
+                </Box>
+              ))}
+
+
+          </Box>
+
+          
+
+
+
+
+
+
+
+
+
+
+        </Box>
+
+      </Container>
+
+
+    </AppBar>
+    
+      
+<Outlet />
+
+
+    
     
     
     </>
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   )
 }
 
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-
-
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const NoPage = () => {
   return (
-    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px'}}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "500px",
+      }}
+    >
       <Box>
-        <Typography variant='h1' sx={{fontWeight: 1000, color: 'red'}}>
-            404, Not found
-
+        <Typography variant="h1" sx={{ fontWeight: 1000, color: "red" }}>
+          404, Not found
         </Typography>
-        
       </Box>
-
-
     </Box>
-    
-  )
-}
+  );
+};
 
-export default NoPage
+export default NoPage;
