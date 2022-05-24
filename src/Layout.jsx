8feb1fc138d007d258c50d0a992f363b58742b77
Ubaldo Barranco <baldous@protonmail.com>
2022-05-23 21:44:34 -0500

    appBar with active links

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+import HomeIcon from "@mui/icons-material/Home";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+const navLinks = [
+  { title: "About", path: "/first" },
+  { title: "Contact", path: "/second" },
+];
 
 const Layout = () => {
   return (
-    <>
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
-          <li>
-            <Link to="/second">Second</Link>
-          </li>
-        </ul>
-      </nav>
+    <Box>
+      <AppBar position="fixed" color="inherit" elevation={1}>
+        <Container maxWidth="xl">
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <IconButton to="/" component={NavLink}>
+                <HomeIcon sx={{ fontSize: 30, color: "black" }} />
+              </IconButton>
+            </Box>
+            <Box>
+              {navLinks.map(({ title, path }) => (
+                <Button
+                  sx={{ fontSize: "1rem", borderRadius: "10px" }}
+                  key={title}
+                  component={NavLink}
+                  to={path}
+                  style={({ isActive }) => ({
+                    color: isActive ? "#fff" : "black",
+                    background: isActive ? "black" : "",
+                  })}
+                >
+                  {title}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+        </Container>
+      </AppBar>
 
       <Outlet />
-    
-    
-    </>
-  )
-}
+    </Box>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px'}}>
       <Box sx={{border: 'solid 10px', padding: '20px', borderRadius: '20px'}}>
         <Typography variant='h1' sx={{fontWeight: 1000}}>
-            Hello, this is home
+            Home
 
         </Typography>
         
